7dfbb1f1ebb4970ee49283b0d1d92af41a48624c
José Moran <jose.moran@polytechnique.org>
2021-09-30 23:24:41 +0100

    Fixed a bug: some entries in 'other tags' did not work properly

--- GOSTnets/load_osm.py
+++ GOSTnets/load_osm.py
@@ -4,7 +4,9 @@
 # Purpose: take an input dataset as a OSM file and return a network object
 ####################################################################################################
 
-import os, sys, time
+import os
+import sys
+import time
 
 import shapely.ops
 
@@ -16,12 +18,13 @@ import networkx as nx
 
 from osgeo import ogr
 from rtree import index
-from shapely import speedups
+from shapely import speedups, ops
 from shapely.geometry import LineString, MultiLineString, MultiPoint, Point
 #from geopy.distance import geodesic
 from geopy import distance
 from boltons.iterutils import pairwise
-from shapely.wkt import loads,dumps
+from shapely.wkt import loads, dumps
+
 
 class OSM_to_network(object):
     """
@@ -43,9 +46,10 @@ class OSM_to_network(object):
         Generate a networkX object from a osm file
         """
         self.osmFile = osmFile
-        self.roads_raw = self.fetch_roads_and_ferries(osmFile) if includeFerries else self.fetch_roads(osmFile)
+        self.roads_raw = self.fetch_roads_and_ferries(
+            osmFile) if includeFerries else self.fetch_roads(osmFile)
 
-    def generateRoadsGDF(self, in_df = None, outFile='', verbose = False):
+    def generateRoadsGDF(self, in_df=None, outFile='', verbose=False):
         """
         post-process roads GeoDataFrame adding additional attributes
 
@@ -57,31 +61,33 @@ class OSM_to_network(object):
             in_df = self.roads_raw
 
         # get all intersections
-        roads = self.get_all_intersections(in_df, unique_id = 'osm_id', verboseness = verbose)
+        roads = self.get_all_intersections(
+            in_df, unique_id='osm_id', verboseness=verbose)
 
         # add new key column that has a unique id
         roads['key'] = ['edge_'+str(x+1) for x in range(len(roads))]
-        np.arange(1,len(roads)+1,1)
+        np.arange(1, len(roads)+1, 1)
 
         def get_nodes(x):
-            return list(x.geometry.coords)[0],list(x.geometry.coords)[-1]
+            return list(x.geometry.coords)[0], list(x.geometry.coords)[-1]
 
         # generate all of the nodes per edge and to and from node columns
-        nodes = gpd.GeoDataFrame(roads.apply(lambda x: get_nodes(x),axis=1).apply(pd.Series))
-        nodes.columns = ['u','v']
+        nodes = gpd.GeoDataFrame(roads.apply(
+            lambda x: get_nodes(x), axis=1).apply(pd.Series))
+        nodes.columns = ['u', 'v']
 
         # compute the length per edge
-        roads['length'] = roads.geometry.apply(lambda x : self.line_length(x))
-        roads.rename(columns={'geometry':'Wkt'}, inplace=True)
+        roads['length'] = roads.geometry.apply(lambda x: self.line_length(x))
+        roads.rename(columns={'geometry': 'Wkt'}, inplace=True)
 
-        roads = pd.concat([roads,nodes],axis=1)
+        roads = pd.concat([roads, nodes], axis=1)
 
         if outFile != '':
             roads.to_csv(outFile)
 
         self.roadsGPD = roads
 
-    def filterRoads(self, acceptedRoads = ['primary','primary_link','secondary','secondary_link','motorway','motorway_link','trunk','trunk_link']):
+    def filterRoads(self, acceptedRoads=['primary', 'primary_link', 'secondary', 'secondary_link', 'motorway', 'motorway_link', 'trunk', 'trunk_link']):
         """
         Extract certain times of roads from the OSM before the netowrkX conversion
 
@@ -89,7 +95,8 @@ class OSM_to_network(object):
         :returns: None - the raw roads are filtered based on the list of accepted roads
         """
 
-        self.roads_raw = self.roads_raw.loc[self.roads_raw.infra_type.isin(acceptedRoads)]
+        self.roads_raw = self.roads_raw.loc[self.roads_raw.infra_type.isin(
+            acceptedRoads)]
 
     def fetch_roads(self, data_path):
         """
@@ -102,7 +109,8 @@ class OSM_to_network(object):
         if data_path.split('.')[-1] == 'pbf':
             driver = ogr.GetDriverByName("OSM")
             data = driver.Open(data_path)
-            sql_lyr = data.ExecuteSQL("SELECT osm_id, highway, other_tags FROM lines WHERE highway IS NOT NULL")
+            sql_lyr = data.ExecuteSQL(
+                "SELECT osm_id, highway, other_tags FROM lines WHERE highway IS NOT NULL")
             roads = []
 
             for feature in sql_lyr:
@@ -112,22 +120,26 @@ class OSM_to_network(object):
                     if shapely_geo is None:
                         continue
                     highway = feature.GetField("highway")
-                                        
+
                     if feature.GetField('other_tags'):
                         other_tags = feature.GetField('other_tags')
+                        elements = other_tags.split(',')
+                        elements = [
+                            element for element in elements if '=>' in element]
                         other_tags_dict = dict((x.strip('"'), y.strip('"'))
-                            for x, y in (element.split('=>') 
-                            for element in other_tags.split(',')))
-            
+                                               for x, y in (element.split('=>')
+                                                            for element in elements))
+
                         if other_tags_dict.get('oneway') == 'yes':
                             one_way = True
                         else:
                             one_way = False
-                            
-                    roads.append([osm_id,highway,one_way,shapely_geo])
+
+                    roads.append([osm_id, highway, one_way, shapely_geo])
 
             if len(roads) > 0:
-                road_gdf = gpd.GeoDataFrame(roads,columns=['osm_id','infra_type', 'one_way','geometry'],crs={'init': 'epsg:4326'})
+                road_gdf = gpd.GeoDataFrame(roads, columns=[
+                                            'osm_id', 'infra_type', 'one_way', 'geometry'], crs={'init': 'epsg:4326'})
                 return road_gdf
 
         elif data_path.split('.')[-1] == 'shp':
@@ -151,7 +163,7 @@ class OSM_to_network(object):
             data = driver.Open(data_path)
             sql_lyr = data.ExecuteSQL("SELECT * FROM lines")
 
-            roads=[]
+            roads = []
 
             for feature in sql_lyr:
                 if feature.GetField('man_made'):
@@ -161,7 +173,7 @@ class OSM_to_network(object):
                         if shapely_geo is None:
                             continue
                         highway = 'pier'
-                        roads.append([osm_id,highway,shapely_geo])
+                        roads.append([osm_id, highway, shapely_geo])
                 elif feature.GetField('other_tags'):
                     if "ferry" in feature.GetField('other_tags'):
                         osm_id = feature.GetField('osm_id')
@@ -169,35 +181,38 @@ class OSM_to_network(object):
                         if shapely_geo is None:
                             continue
                         highway = 'ferry'
-                        roads.append([osm_id,highway,shapely_geo])
+                        roads.append([osm_id, highway, shapely_geo])
                     elif feature.GetField('highway') is not None:
                         osm_id = feature.GetField('osm_id')
                         shapely_geo = loads(feature.geometry().ExportToWkt())
                         if shapely_geo is None:
                             continue
                         highway = feature.GetField('highway')
-                        roads.append([osm_id,highway,shapely_geo])
+                        roads.append([osm_id, highway, shapely_geo])
                 elif feature.GetField('highway') is not None:
                     osm_id = feature.GetField('osm_id')
                     shapely_geo = loads(feature.geometry().ExportToWkt())
                     if shapely_geo is None:
                         continue
                     highway = feature.GetField('highway')
-                    roads.append([osm_id,highway,shapely_geo])
+                    roads.append([osm_id, highway, shapely_geo])
 
             data = driver.Open(data_path)
-            sql_lyr_ferries = data.ExecuteSQL("SELECT * FROM multipolygons WHERE multipolygons.amenity = 'ferry_terminal'")
+            sql_lyr_ferries = data.ExecuteSQL(
+                "SELECT * FROM multipolygons WHERE multipolygons.amenity = 'ferry_terminal'")
 
             for feature in sql_lyr_ferries:
                 osm_id = feature.GetField('osm_id')
-                shapely_geo = ops.linemerge(loads(feature.geometry().ExportToWkt()).boundary)
+                shapely_geo = ops.linemerge(
+                    loads(feature.geometry().ExportToWkt()).boundary)
                 if shapely_geo is None:
                     continue
                 highway = 'pier'
-                roads.append([osm_id,highway,shapely_geo])
+                roads.append([osm_id, highway, shapely_geo])
 
             if len(roads) > 0:
-                road_gdf = gpd.GeoDataFrame(roads,columns=['osm_id','infra_type','geometry'],crs={'init': 'epsg:4326'})
+                road_gdf = gpd.GeoDataFrame(
+                    roads, columns=['osm_id', 'infra_type', 'geometry'], crs={'init': 'epsg:4326'})
                 return road_gdf
 
         elif data_path.split('.')[-1] == 'shp':
@@ -217,14 +232,15 @@ class OSM_to_network(object):
         """
 
         if line.geometryType() == 'MultiLineString':
-            return sum(line_length(segment) for segment in line)
+            return sum(self.line_length(segment) for segment in line)
 
         return sum(
-                    distance.geodesic(tuple(reversed(a)), tuple(reversed(b)), ellipsoid=ellipsoid).km
-                    for a, b in pairwise(line.coords)
+            distance.geodesic(tuple(reversed(a)), tuple(
+                reversed(b)), ellipsoid=ellipsoid).km
+            for a, b in pairwise(line.coords)
         )
 
-    def get_all_intersections(self, shape_input, idx_osm = None, unique_id = 'osm_id', verboseness = False):
+    def get_all_intersections(self, shape_input, idx_osm=None, unique_id='osm_id', verboseness=False):
         """
         Processes GeoDataFrame and splits edges as intersections
 
@@ -238,7 +254,7 @@ class OSM_to_network(object):
         idx_inters = index.Index()
         # Load data
         # all_data = dict(zip(list(shape_input.osm_id),list(shape_input.geometry),list(shape_input.infra_type)))
-        ### TODO - it shouldn't be necessary to reference the geometry column specifically
+        # TODO - it shouldn't be necessary to reference the geometry column specifically
         #   ... but here we are
 
         if idx_osm is None:
@@ -253,7 +269,7 @@ class OSM_to_network(object):
         allCounts = []
 
         for idx, row in shape_input.iterrows():
-            #print(row)
+            # print(row)
             key1 = row[f'{unique_id}']
             line = row.geometry
             infra_type = row.infra_type
@@ -262,8 +278,10 @@ class OSM_to_network(object):
                 print("Processing %s of %s" % (count, tLength))
                 print('seconds elapsed: ' + str(time.time() - start))
             count += 1
-            intersections = shape_input.iloc[list(idx_osm.intersection(line.bounds))]
-            intersections = dict(zip(list(intersections[f'{unique_id}']),list(intersections.geometry)))
+            intersections = shape_input.iloc[list(
+                idx_osm.intersection(line.bounds))]
+            intersections = dict(
+                zip(list(intersections[f'{unique_id}']), list(intersections.geometry)))
             if key1 in intersections:
                 intersections.pop(key1)
             # Find intersecting lines
@@ -285,17 +303,19 @@ class OSM_to_network(object):
                             idx_inters.insert(0, pt.bounds, pt)
 
             # cut lines where necessary and save all new linestrings to a list
-            hits = [n.object for n in idx_inters.intersection(line.bounds, objects=True)]
+            hits = [n.object for n in idx_inters.intersection(
+                line.bounds, objects=True)]
 
             if len(hits) != 0:
                 try:
                     out = shapely.ops.split(line, MultiPoint(hits))
-                    new_lines.append([{'geometry': LineString(x), 'osm_id':key1,'infra_type':infra_type, 'one_way':one_way} for x in out.geoms])
+                    new_lines.append([{'geometry': LineString(
+                        x), 'osm_id': key1, 'infra_type': infra_type, 'one_way': one_way} for x in out.geoms])
                 except:
                     pass
             else:
-                new_lines.append([{'geometry': line, 'osm_id':key1,
-                        'infra_type':infra_type,'one_way':one_way}])
+                new_lines.append([{'geometry': line, 'osm_id': key1,
+                                   'infra_type': infra_type, 'one_way': one_way}])
 
         # Create one big list and treat all the cutted lines as unique lines
         flat_list = []
@@ -312,8 +332,8 @@ class OSM_to_network(object):
                     all_data[i] = item
 
         # Transform into geodataframe and add coordinate system
-        full_gpd = gpd.GeoDataFrame(flat_list, geometry ='geometry')
-        full_gpd.crs = {'init' :'epsg:4326'}
+        full_gpd = gpd.GeoDataFrame(flat_list, geometry='geometry')
+        full_gpd.crs = {'init': 'epsg:4326'}
 
         return(full_gpd)
 
@@ -343,22 +363,22 @@ class OSM_to_network(object):
         def convert(x):
             u = x.u
             v = x.v
-            data = {'Wkt':x.Wkt,
-                   'id':x.id,
-                   'infra_type':x.infra_type,
-                   'one_way':x.one_way,
-                   'osm_id':x.osm_id,
-                   'key': x.key,
-                   'length':x.length}
+            data = {'Wkt': x.Wkt,
+                    'id': x.id,
+                    'infra_type': x.infra_type,
+                    'one_way': x.one_way,
+                    'osm_id': x.osm_id,
+                    'key': x.key,
+                    'length': x.length}
             return (u, v, data)
 
-        edge_bunch = edges.apply(lambda x: convert(x), axis = 1).tolist()
+        edge_bunch = edges.apply(lambda x: convert(x), axis=1).tolist()
 
         G = nx.MultiDiGraph()
         G.add_nodes_from(node_bunch)
         G.add_edges_from(edge_bunch)
 
-        for u, data in G.nodes(data = True):
+        for u, data in G.nodes(data=True):
             if type(u) == str:
                 q = tuple(float(x) for x in u[1:-1].split(','))
             if type(u) == tuple:
